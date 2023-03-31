041efcf70c73822cce09180bc1b286cf0eeb79d8
Tufaarf <daffa.tfail@gmail.com>
2023-03-31 09:16:39 +0700

    update bobot fix

--- PPLGPAKSHINA/src/jaraktempuh.java
+++ PPLGPAKSHINA/src/jaraktempuh.java
@@ -17,11 +17,12 @@ public class jaraktempuh {
         // menghitung konsumsi bensin
         double hargaBBM = 14500;    //harga bensin per liter 
         final double konsumsBBM = 1;     //setiap km memakai bensin 1 liter
-        double totalBensin = jarak / konsumsBBM * ( 3 + bobot / 100); //pemakaian BBM dalam liter, dan setiap bobot bertambah maka 3% dari bobot akan menambah konsumsi BBM sebanyak 3%
-        double totHargaBBM = totalBensin * hargaBBM; //total harga bensin nya 
+        double totalBensin = jarak / konsumsBBM;
+        double bensinBeban = totalBensin + (totalBensin * (3/100)); //pemakaian BBM dalam liter, dan setiap bobot bertambah maka 3% dari bobot akan menambah konsumsi BBM sebanyak 3%
+        double totHargaBBM = bensinBeban * hargaBBM; //total harga bensin nya 
 
 
         // output
-        JOptionPane.showMessageDialog(null, "Anda Membawa Muatan Seberat : " + bobot + ".KG" + "\nWaktu Tempuhnya :  " + waktu + "\nTotal Konsumsi BBM : " + totalBensin + "\nTotal Biaya BBM Adalah : Rp. " + totHargaBBM);
+        JOptionPane.showMessageDialog(null, "Anda Membawa Muatan Seberat : " + bobot + ".KG" + "\nWaktu Tempuhnya :  " + waktu + " Jam" + "\nTotal Konsumsi BBM : " + bensinBeban + " Liter" + "\nTotal Biaya BBM Adalah : Rp. " + totHargaBBM);
 }
 }
